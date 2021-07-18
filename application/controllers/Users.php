feb401d89249faf6bec900b42353cd211a637acb
Izzy <izzytimika@gmail.com>
2021-07-19 01:53:53 +0900

    update new

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -33,37 +33,7 @@ class Users extends CI_Controller
 	
 	public function testmail()
 	{
-			require APPPATH . '/libraries/class.phpmailer.php';
-			$mail = new PHPMailer(true);
-
-			try {
-				//Server settings
-				$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-				$mail->isSMTP();                                            //Send using SMTP
-				$mail->Host       = 'smtp.google.com';                     //Set the SMTP server to send through
-				$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-				$mail->Username   = 'fauzyc11@gmail.com';                     //SMTP username
-				$mail->Password   = 'braindestoryer';                               //SMTP password
-				$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-				$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-				//Recipients
-				$mail->setFrom('fauzyc11@gmail.com', 'Mailer');
-				$mail->addAddress('hamzahfauzy97@gmail.com', 'Joe User'); 
-
-				//Content
-				$mail->isHTML(true);                                  //Set email format to HTML
-				$mail->Subject = 'Here is the subject';
-				$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-				$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-				$mail->send();
-				echo 'Message has been sent';
-			} catch (Exception $e) {
-				echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-			}
-		
-		
+		echo 1;
 	}
 	
 	public function resetp($id)
